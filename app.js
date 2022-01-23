06fc3c500f0b6b777300d9015a9b72449c7a5521
jaba777 <tskhovreba777@gmail.com>
2022-01-23 08:43:20 +0400

    build js

---
+++ app.js
@@ -0,0 +1,17 @@
+const hex = ['red','green','cyan','yellow','black','blue'];
+const BTN = document.getElementById('btn');
+const Color = document.querySelector('.color');
+
+function GetCount(){
+    const Randomnumber=getColor();
+    let hexColor=hex[Randomnumber];
+  Color.textContent = hexColor;
+  Color.style.color=hexColor;
+  document.body.style.background = hexColor;
+}
+
+function getColor(){
+  return Math.floor(Math.random() * hex.length);
+}
+
+BTN.addEventListener('click', GetCount);
